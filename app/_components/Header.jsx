ebf0c1cc759b014ac34cde00e8901927ff853add
Sane <sjmedia@Sanes-MacBook-Air.local>
2024-10-18 22:40:06 +0200

    firstommit

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -6,7 +6,9 @@ export default function Header() {
   return (
     <div className='flex justify-between p-5 shadow-sm'>
        <Image src={'/fundai.png'} width={160} height={100}/>
-        <Button >Get Started</Button>
+       <Link href="/sign-in">
+       <Button >Get Started</Button> 
+       </Link>
     </div>
   )
 }
--- app/dashboard/_components/UserCourseList.jsx
+++ app/dashboard/_components/UserCourseList.jsx
@@ -37,7 +37,7 @@ export default function UserCourseList() {
 
             </div>
           ))
-      }
+           }
       </div>
     </div>
   )
